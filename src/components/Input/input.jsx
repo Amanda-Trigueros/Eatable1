4c7fc345615e999948cf6ac016aa418fd7b4df6a
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-07-07 13:09:46 -0600

    chore: update input

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,16 +1,37 @@
 import PropTypes from "prop-types";
-import { Container, StyledInput, StyledLabel } from "./styles"
+import { StyledInput, StyledLabel } from "./styles";
 
-function Input({ label, ...rest }) {
+function Input({
+  id,
+  type = "text",
+  name,
+  value,
+  onChange,
+  placeholder,
+  label,
+}) {
   return (
-    <Container>
-      {label && <StyledLabel>{label}</StyledLabel>}
-      <StyledInput {...rest} />
-    </Container>
+    <div>
+      {label ? <StyledLabel htmlFor={id || name}>{label}</StyledLabel> : ""}
+      <StyledInput
+        id={id || name}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
   );
 }
 
 Input.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
   label: PropTypes.string,
 };
 
