32fae80cf9a451ef802acf3396ab98f69b59b989
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-07-07 11:31:04 -0600

    chore: add fonts

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,7 +1,7 @@
 export const fonts = {
-  primary: `"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+  primary: `"Source Sans 3", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
   Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
-  secondary: `"Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI",
+  secondary: `-apple-system, BlinkMacSystemFont, "Segoe UI",
   Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
 };
 
